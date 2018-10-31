f2db58f93da569fc68f11fb386a3c6155d56cfc5
Iain Reid <iainreid382@gmail.com>
2018-10-31 16:51:29 +0000

    Add typings for TS use

---
+++ lib/harass.d.ts
@@ -0,0 +1,16 @@
+declare namespace harass {
+  interface HarassInstance {
+    <TResult>(target: () => TResult): () => Promise<TResult>
+    <T1, TResult>(target: (t1: T1) => TResult): (t1: T1) => Promise<TResult>
+    <T1, T2, TResult>(target: (t1: T1, t2: T2) => TResult): (t1: T1, t2: T2) => Promise<TResult>
+    <T1, T2, T3, TResult>(target: (t1: T1, t2: T2, t3: T3) => TResult): (t1: T1, t2: T2, t3: T3) => Promise<TResult>
+    <T1, T2, T3, T4, TResult>(target: (t1: T1, t2: T2, t3: T3, t4: T4) => TResult): (t1: T1, t2: T2, t3: T3, t4: T4) => Promise<TResult>
+    <T1, T2, T3, T4, T5, TResult>(target: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => TResult): (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => Promise<TResult>
+    <T1, T2, T3, T4, T5, T6, TResult>(target: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => TResult): (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => Promise<TResult>
+    <T1, T2, T3, T4, T5, T6, T7, TResult>(target: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => TResult): (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => Promise<TResult>
+    <T1, T2, T3, T4, T5, T6, T7, T8, TResult>(target: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => TResult): (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => Promise<TResult>
+  }
+}
+
+declare function harass(times: number): harass.HarassInstance
+export = harass;
